e38c88a9c2955071c392922cae611dfdcf090f66
Mridul Singh <msmridulsingh@gmail.com>
2019-04-01 14:23:45 +0530

    Create water.cpp
    CS50 PSet 1 Q2

---
+++ water.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int time;
+	cout<<"Enter the duration of your shower ";
+	cin>>time;
+	cout<<"The bottle water equivalent is "<<time*12;
+}
